62347eccbc581dbdb24a41cbaf55273c974502b2
Ken Spencer <ken@electrocode.net>
2017-05-09 09:44:57 -0400

    More changes

--- lib/chancontrol.rb
+++ lib/chancontrol.rb
@@ -2,28 +2,75 @@ require "cinch"
 require "yaml"
 require "recursive-open-struct"
 # @note ChanControl Plugin
+
 class ChanControl
   include Cinch::Plugin
   include Cinch::Extensions::Authentication
+
   # Write to the config
   # @param [Hash] the data to write
+
   def toFile(msg)
-      data = msg
-      File.open(`echo ~/.gitlab-rc.yml`.chomp!, "w") {|f| f.write(data.to_yaml) }
+    data = msg
+    File.open(`echo ~/.gitlab-rc.yml`.chomp!, "w") {|f| f.write(data.to_yaml) }
   end
+
   # Load the config
   # @return [Hash] load the current config
+
   def deFile()
     begin
       parsed = YAML.load(File.open(`echo ~/.gitlab-rc.yml`.chomp!, "r"))
     rescue ArgumentError => e
       puts "Could not parse YAML: #{e.message}"
     end
+    return parsed
   end
 
   match /add (\S)/, :method => :add
+  match /rem (\S)/, :method => :rem
+  match /list/, :method => :listchans
+
   def add(m, channel)
     return unless authenticated? m
-
+    config = deFile
+    if config.has_key? "#{channel}"
+      if Channel(channel)
+        m.reply "#{channel} already exists in the config."
+      else
+        m.reply "Wasn't joined to #{channel}, joining now."
+        Channel(channel).join
+      end
+    else
+      m.reply "Joining #{channel}"
+      Channel(channel).join
+      config['channels'] << channel
+      toFile
+    end
   end
+
+  # @param [Message] Message Object
+  # @param [String] name of channel
+  def rem(m, channel)
+    return unless authenticated? m
+    config = deFile
+    config['channels'].delete channel
+    Channel(channel).part
+    m.reply "Channel Removed."
+    toFile(config)
+  end
+
+  def listchans(m)
+    return unless authenticated? m
+    config = deFile
+    channels = config['channels']
+    channels.each do |n|
+      def joined (chan)
+        if Channel(chan)
+          return true
+        else
+          return false
+        end
+      end
+      n.reply "Channel: #{channel} / Joined? #{joined(channel)}"
 end
